8eb4d145469346f974f22746a24a46abb384bdba
thaunghtike-share <zwe.zzzin@gmail.com>
2025-07-15 12:06:33 +0630

    update pagination

--- blogapis/settings.py
+++ blogapis/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 25,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
